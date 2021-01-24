28a1a165a380f2a772050662a15e212e97f67eb8
varvara <varia.mazurovaa@mail.ru>
2021-01-24 20:32:55 +0300

    he

---
+++ string.py
@@ -0,0 +1,18 @@
+# a = input('введите пароль ')
+# b = 17
+# result = 0
+# try:
+#     a = int(a)
+#     result = b/a
+# except ZeroDivisionError:
+#     print('пароль пуст')
+# try:
+#     numbers = int(a)
+# except ValueError:
+#     print('пароль состоит из букв')
+# print('good')
+a = [1, 4, 6, 0]
+print(sum(a))
+
+
+
